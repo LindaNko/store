f041c4d2b32bc5383b732ea033cda208b417605f
Linda Nkosi <lindako63@outlook.com>
2025-07-18 13:25:59 +0200

    api

--- src/main/java/com/apwithlink/OrderService.java
+++ src/main/java/com/apwithlink/OrderService.java
@@ -2,10 +2,21 @@ package com.apwithlink;
 
 import com.apwithlink.store.PaymentService;
 import com.apwithlink.store.StripePaymentService;
+import com.apwithlink.OrderService;
+import com.apwithlink.OrderService;
+
+
 
 public class OrderService {
+
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService ){
+        this.paymentService =paymentService;
+    }
+
     public void placeOrder(){
-        PaymentService paymentService = new StripePaymentService();
+     
         paymentService.processPayment(100);
 
     }
--- src/main/java/com/apwithlink/store/StoreApplication.java
+++ src/main/java/com/apwithlink/store/StoreApplication.java
@@ -2,12 +2,17 @@ package com.apwithlink.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.apwithlink.OrderService;
+
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
+		///SpringApplication.run(StoreApplication.class, args);
+
+		var OrderService = new OrderService(new StripePaymentService());
+		OrderService.placeOrder();
 	}
 
 }
